6333d8005998680c2942b93f09ee0b05554b9464
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-04-19 18:03:40 -0600

    Correccion de errores

--- 05Clases/Ejercicios.java
+++ 05Clases/Ejercicios.java
@@ -21,18 +21,47 @@ public class Ejercicios {
             case 1:
                 suma();
                 break;
-        
+            case 2:
+                resta();
+                break;
+            case 3:
+                multiplicar();
+                break;
+            case 4;
+                System.out.println("Saliendo.....");
+                break;
             default:
+                System.out.println("Opcion invalida");
                 break;
         }
     }
     public void suma(){
-        double num1 = 0.00, num2 = 0.00;
-        System.out.println("Ingresa el numero que deseas sumar");
+        double num1 = 0.00, num2 = 0.00, resultado = 0.00;
+        System.out.println("Ingresa el número que deseas sumar");
         num1 = entrada.nextDouble();
-        System.out.println("Ingresa el numero que deseas sumar");
+        System.out.println("Ingresa el número que deseas sumar");
         num2 = entrada.nextDouble();
             resultado = num1 + num2;
             System.out.println("La suma es: "+resultado);
+    }
+    public void resta(){
+        double num1 = 0.00, num2 = 0.00, resultado = 0.00;
+        System.out.println("Ingresa el número que deseas restar");
+        num1 = entrada.nextDouble();
+        System.out.println("Ingresa el número que deseas restar");
+        num2 = entrada.nextDouble();
+            resultado = num1 - num2;
+            System.out.println("La resta es:"+resultado);
+    }
+    public void multiplicación() {
+        double num1 = 0.00, num2 = 0.00, resultado = 0.00;
+        System.out.println("Ingresa el número que deseas multiplicar");
+        num1 = entrada.nextDouble();
+        System.out.println("Ingresa el número que deseas multiplicar");
+        num2 = entrada.nextDouble();
+            resultado = num1 * num2;
+            System.out.println("La multiplicación es: "+resultado);
+    }
+    public static void main(String[] args) {
     }
 }
