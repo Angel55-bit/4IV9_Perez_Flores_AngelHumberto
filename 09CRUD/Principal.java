2997587e1844e41fd7727bc6f6c85ded297b7820
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-05 21:26:58 -0600

    Registrar usuario

--- 09CRUD/Principal.java
+++ 09CRUD/Principal.java
@@ -15,7 +15,44 @@ public class Principal {
             System.out.println("4.-Buscar al estudiante");
             System.out.println("5.-Mostrar la lista de todos los alumnos");
             System.out.println("6.-Salir");
+            System.out.print("Selecciona una opcion: ");
             opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingresa Id: ");
+                    int id = entrada.nextInt();
+                    entrada.nextLine();
+                    System.out.print("Ingresa Nombre: ");
+                    String nom = entrada.nextLine();
+                    System.out.print("Ingresa edad: ");
+                    int edad = entrada.nextInt();
+                    System.out.print("Ingresa Promedio: ");
+                    double promedio = entrada.nextDouble();
+
+                    lista.add(new Estudiante(id, nom, edad, promedio))
+                    System.out.println("Estudiante guardado"); 
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+            
+                default:
+                    break;
+            }
         }
     }
 }
