61ef980c81cc17a017db1e59a55fb58ebfd82653
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-05 19:25:48 -0600

    El codigo

--- 08Herencia/gato.java
+++ 08Herencia/gato.java
@@ -11,4 +11,17 @@ public class gato extends animal {
         super(nombre, raza, tipo_alimento, edad);
         this.num_vidas = num_vidas;
     }
+    
+    public int getNum_vidas(){
+        return num_vidas;
+    }
+
+    public void setNum_vidas(int num_vidas){
+        this.num_vidas = num_vidas;
+    }
+
+    //metodo propio del señor gato
+    public void mostrarGato(){
+        System.out.println("El nombre del michi es: "+ getNombre() + "\n"+ "Su raza es: " + getRaza() + "\n" +"Su comida es: " + getTipo_alimento() + "\n" +" Su edad es: " + getEdad() + "\n"+ "El num de vidas que le resta son: " + num_vidas + "\n")
+    }
 }
---
+++ 08Herencia/principal.java
@@ -0,0 +1,7 @@
+public class principal {
+    public static void main(String[] args) {
+        gato cat = new Gato ("Bolita", "Siames", "Croquetas", 1, 2);
+
+        cat.mostrarGato();
+    }
+}
