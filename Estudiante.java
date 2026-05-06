908b04d8c9e1840067d5be277690d914a6660ec3
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-05 19:37:15 -0600

    Constructor ya guardado

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,16 @@
 public class Estudiante {
+    private int id; 
+    private String nombre;
+    private int edad; 
+    private double promedio;
+
+    //realizo el contructor
+    public Estudiante(int id, String nombre, int edad, double promedio){
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.promedio = promedio;
+    }
+
     
 }
