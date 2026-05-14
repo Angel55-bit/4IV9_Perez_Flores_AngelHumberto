84f48a4750fa064c3036d6903cb38203bbec9c4a
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-14 17:55:36 -0600

    Estudiante

--- Examen02/Estudiante.java
+++ Examen02/Estudiante.java
@@ -1,3 +1,30 @@
-public class Estudiante {
+public class Estudiante extends Persona {
+    private int id;
+    private double promedio;
+
+    public Estudiante(){
+
+    }
+
+    public Estudiante(String nombre, int edad, int id, double promedio){
+        super(nombre, edad);
+        this.id = id;
+        this.promedio = promedio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void mostrarEstudiante() {
+        System.out.println("Id: " + id + "Nombre: " + getNombre() + "Edad: " + getEdad() + "Promedio: " + promedio);
+    }
 }
