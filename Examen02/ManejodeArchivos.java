b1e4824993b5bb457ed7ea61f7a8421b1e3ab6ea
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-14 18:54:37 -0600

    Archivos listo

--- Examen02/ManejodeArchivos.java
+++ Examen02/ManejodeArchivos.java
@@ -1,3 +1,26 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class ManejodeArchivos {
+    public void guardar(ArrayList lista, String nombre) {
+        try{
+            ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(nombre));
+            salida.writeObject(lista);
+            salida.close();
+        }catch (IOException e) {
+            System.out.println("Erros al guardar: " + e.getMessage());
+        }
+    }
     
+    public ArrayList leer(String nombre){
+        ArrayList lista = new ArrayList();
+        try {
+            ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(nombre));
+            lista = (ArrayList) entrada.readObject();
+            entrada.close();
+        }catch (Exception e){
+
+        }
+        return lista;
+    }
 }
