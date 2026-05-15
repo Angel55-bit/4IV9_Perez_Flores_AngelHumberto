49945e358375226c91880b190b009663d4537d6d
Angel55-bit <aperezf2400@alumno.ipn.mx>
2026-05-14 18:27:40 -0600

    Profesor listo

--- Examen02/Profesor.java
+++ Examen02/Profesor.java
@@ -1,3 +1,35 @@
 public class Profesor {
-    
+    private String numProfesor;
+    private String materia;
+
+    public Profesor() {
+
+    }
+
+    public Profesor(String nombre, int edad, String numProfesor, String materia) {
+        super(nombre, edad);
+        this.numProfesor = numProfesor;
+        this.materia = materia;
+    }
+
+    public String getnumProfesor() {
+        return numProfesor;
+    }
+
+    public void setnumProfesor(String numProfesor) {
+        this.numProfesor;
+    }
+
+    public String getmateria() {
+        return materia;
+    }
+
+    public void setmateria(String materia) {
+        this.materia = materia;
+    }
+
+    public void mostrarProfesor() {
+        System.out.println("Numero de Profesor: " + numProfesor + "Nombre: " + getNombre() + "Edad: " + getEdad() + "Materia: " + materia);
+        
+    }
 }
